d68f6a908975950b51605d5be912bfeb815cf68f
Antonio Alan <tonio.alan07@gmail.com>
2022-08-11 10:46:59 -0300

    database configuration

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ const handlebars = require('express-handlebars')
 const app = express()
 const admin = require('./routes/admin')
 const path = require('path')
-// const mongoose = require("mongoose")
+const mongoose = require("mongoose")
 
 //config
-    //Body Parser
     app.use(express.urlencoded({extended:true}))
     app.use(express.json())
     //Handlebars
     app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars');
     //Mongoose
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost/blogapp').then(() => {
+        console.log("connected with mongoDB")
+    }).catch((err) => {
+        console.log("error connecting: "+err)
+    })
     //Public
     app.use(express.static(path.join(__dirname,'public')))
     
