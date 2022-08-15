02e4aaecf8067c3466c0cd2d600cd20e291ccc2b
Antonio Alan <tonio.alan07@gmail.com>
2022-08-15 15:26:25 -0300

    post form saved in database

--- models/Post.js
+++ models/Post.js
@@ -4,28 +4,28 @@ const Schema = mongoose.Schema;
 const Post = new Schema({
     title:{
         type: String,
-        required: true
+        
     },
     slug:{
         type: String,
-        required: true
+        
     },
     description:{
         type: String,
-        required: true
+        
     },
-    contents:{
+    content:{
         type: String,
-        required: true
+        
     },
     category:{
         type: Schema.Types.ObjectId,
         ref: "categories",
-        required: true
+        
     },
     date:{
         type: Date,
         default: Date.now()
     }
 })
-mongoose.model("posts", Posts)
+mongoose.model("posts", Post)
--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const mongoose = require("mongoose")
 require("../models/Category")
 const Category = mongoose.model("categories")
+require("../models/Post")
+const Post = mongoose.model("posts")
 
 router.get('/', (req,res) => {
     res.render("admin/index")
@@ -123,4 +125,30 @@ router.get("/posts/add", (req,res) => {
     })
 })
 
+router.post("/posts/new", (req,res) => {
+    
+    let error = []
+
+    if(req.body.category == "0"){
+        error.push({text: "Invalid category, register a category"})
+    }
+    if(error.length > 0){
+        res.render("admin/addPosts", {error: error})
+    }else{
+        const newPost = {
+            title: req.body.title,
+            slug: req.body.slug,
+            description: req.body.description,
+            content: req.body.content,
+            category: req.body.category
+        }
+        new Post(newPost).save().then(()=>{
+            req.flash("success_msg", "Post created successfully")
+            res.redirect("/admin/posts")
+        }).catch((err) => {
+            req.flash("error_msg", "There was an error while saving the post")
+            res.redirect("/admin/posts")
+        })
+    }
+})
 module.exports = router
