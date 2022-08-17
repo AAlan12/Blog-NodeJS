c04fa5f0cc2312c5538a2b9213d0dd7bf21ca80d
Antonio Alan <tonio.alan07@gmail.com>
2022-08-16 21:21:08 -0300

    post page setup

--- app.js
+++ app.js
@@ -48,6 +48,19 @@ app.get("/", (req,res) => {
         res.redirect("/404")
     })
 })
+app.get("/post/:slug", (req,res) => {
+    Post.findOne({slug: req.params.slug}).lean().then((post) => {
+        if(post){
+            res.render("post/index", {post:post})
+        }else{
+            req.flash("error_msg", "This post does not exist")
+            res.redirect("/")
+        }
+    }).catch((err) => {
+        req.flash("error_msg", "There was an internal error")
+        res.redirect("/")
+    })
+})
 app.get("/404", (req, res) => {
     res.send("error 404")
 })
